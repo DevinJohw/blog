c84c062888b30de606113331a0d0978ef426eb64
Devin Johw <devin.johw@gmail.com>
2024-07-28 17:41:49 +0800

    add navbar and profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return <main>This is main!</main>;
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Devin</span>
+        </span>
+      </p>
+    </main>
+  );
 }
